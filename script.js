15a4a8b99484613c582a33eacf62eb0a8d5eeb34
Ashutosh Agrawal <50054271+AshutoshAgrawal2004@users.noreply.github.com>
2019-10-10 18:56:11 +0530

    Displaying Direct Links to original pages

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const getComic = async id => {
-	//Generate a random number that will be the comic number
-	let randomComicId = Math.floor(Math.random() * 2213) + 1;
-	//get the comic data from the xkcd api and parse it into json
-	const res = await fetch(
-		`https://cors-anywhere.herokuapp.com/https://xkcd.com/${randomComicId}/info.0.json`
-	);
-	const data = await res.json();
-	//make img, title and container and add it into your html
-	let $comic = $('<img>', { src: data.img, alt: data.alt });
-	let $title = $('<h4>')
-		.append($('<a>', { href: `https://xkcd.com/${randomComicId}/` }))
-		.text(data.safe_title);
-	let $comicContainer = $('<div>', { class: 'comic-container' })
-		.append($title)
-		.append($comic);
-	$('.img-container').append($comicContainer);
+    //Generate a random number that will be the comic number
+    let randomComicId = Math.floor(Math.random() * 2213) + 1;
+    //get the comic data from the xkcd api and parse it into json
+    const res = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://xkcd.com/${randomComicId}/info.0.json`
+    );
+    const data = await res.json();
+    //make img, title and container and add it into your html
+    let $comic = $('<img>', { src: data.img, alt: data.alt });
+    let $link = $('<a>', {href: `https://xkcd.com/${randomComicId}/`}).text(data.safe_title);
+    let $title = $('<h4>').append($link);
+    let $comicContainer = $('<div>', {class:'comic-container'}).append($title).append($comic);
+    $('.img-container').append($comicContainer);
 };
 
-$(document).ready(() => {
-	getComic();
-	setInterval(() => {
-		getComic();
-	}, 5000);
-});
+$(document).ready(()=>{
+    
+getComic();
+setInterval(() => {
+    getComic();
+}, 5000);
+})
